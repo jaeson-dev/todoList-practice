8a8f20149214af42e13cdb9b3e82ebaa7c81f6e3
jaeson-dev <jaeson.dev@gmail.com>
2022-03-14 08:28:28 +0900

    Initial Commit

---
+++ TodoList_tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList_tutorial
+//
+//  Created by Jaeson.dev on 2022/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoList_tutorial/TodoList_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_tutorialApp.swift
+//  TodoList_tutorial
+//
+//  Created by Jaeson.dev on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
